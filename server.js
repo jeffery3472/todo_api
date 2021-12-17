d9feecbc4f05f86c1b60e9eeefaf6d1570e945e6
jeffery3472 <jessejeff1489@gmail.com>
2021-12-17 02:53:15 -0800

    added a script to start a server

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const { request, response } = require('express');
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const PORT =process.env.PORT || 1479;
 //importing mongoose
 const mongoose = require('mongoose');
 //importing todoController
 const todoController = require('./controllers/todoController')
 
 app.use(express.json());
+app.get('/',(request,response)=>{
+    response.status(200).json({message:'Hello welcome to Jeffery todo_api'});
+});
 
 app.post('/todo',todoController.addTodo);
 app.get('/todo',todoController.getAllTodo);
@@ -16,8 +20,8 @@ app.delete('/todo/:todoId',todoController.deleteById);
 app.get('/todo/:todoId',todoController.getTodoById);
 
 //listening to request on localhost port 1489
-app.listen(1479,() => {
-    console.log("My server is Up and running  ");
+app.listen(PORT,() => {
+    console.log("My server is Up and running on port:",PORT);
     //connecting to the database
     mongoose.connect(process.env.DB_URL)
     .then(function(){
